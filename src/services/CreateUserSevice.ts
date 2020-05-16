35e1bf15ec0079833854a0aa96f7a778a517beef
unknown <fernandoselvino@gmail.com>
2020-05-16 12:18:50 -0300

    Criptografia de senha

--- src/services/CreateUserSevice.ts
+++ src/services/CreateUserSevice.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -21,11 +22,15 @@ class CreateUserService {
       throw new Error('Email address already used');
     }
 
+    const hashedPassword = await hash(password, 8);
+
+    console.log(hashedPassword);
+
     // cria o usuario
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     // persiste o usuario no banco

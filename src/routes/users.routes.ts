4d86747e5fa4b6139434e7331f76f24d70031c74
unknown <fernandoselvino@gmail.com>
2020-05-16 19:57:49 -0300

    Upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserSevice';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig);
 
 // rota de criar usuarios
 usersRoutes.post('/', async (request, response) => {
@@ -23,4 +27,13 @@ usersRoutes.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 export default usersRoutes;

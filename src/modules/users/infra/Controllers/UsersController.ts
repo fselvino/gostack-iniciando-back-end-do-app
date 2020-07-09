d61f439ab54f04164860809d2db7630759a73ce2
unknown <fernandoselvino@gmail.com>
2020-07-08 22:20:42 -0300

    Utilizando Class Transformer

--- src/modules/users/infra/Controllers/UsersController.ts
+++ src/modules/users/infra/Controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserSevice';
 
@@ -16,7 +17,7 @@ export default class UsersController {
     });
 
     delete user.password;
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   // public async index(request: Request, response: Response): Promise<Response> {

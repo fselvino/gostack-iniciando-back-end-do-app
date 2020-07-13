7dc7df10a249301682d0bdaebaf703b4d79d0434
unknown <fernandoselvino@gmail.com>
2020-07-12 21:16:45 -0300

    Cache lista de providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,8 @@ import User from '@modules/users/infra/typeorm/entities/User';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRespository';
 
+import ICacheProvider from '@shared/container/provider/CacheProvider/models/ICacheProvider';
+
 interface IRequest {
   user_id: string;
 }
@@ -13,13 +15,26 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      console.log('A query no banco foi feita');
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
+    console.log(users);
     return users;
   }
 }
--- src/shared/container/provider/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/provider/CacheProvider/models/ICacheProvider.ts
@@ -2,7 +2,7 @@ import { strict } from 'assert';
 import { string } from '@hapi/joi';
 
 export default interface ICacheProvider {
-  save(key: string, value: string): Promise<void>;
-  recover(key: string): Promise<string | null>;
+  save(key: string, value: any): Promise<void>;
+  recover<T>(key: string): Promise<T | null>;
   invalidate(Key: string): Promise<void>;
 }

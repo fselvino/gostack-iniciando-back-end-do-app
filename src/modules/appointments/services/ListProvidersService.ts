9f9058476be9755ab2c642220b451c4d75a6a0ef
unknown <fernandoselvino@gmail.com>
2020-08-30 15:57:39 -0300

    update ormconfig

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -21,9 +22,12 @@ class ListProvidersService {
   ) { }
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(
-      `providers-list:${user_id}`,
-    );
+    // let users = await this.cacheProvider.recover<User[]>(
+    //   `providers-list:${user_id}`,
+    // );
+
+    // limpar o cache
+    let users;
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
@@ -32,7 +36,10 @@ class ListProvidersService {
 
       // console.log('A query no banco foi feita');
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
     // console.log(users);
     return users;

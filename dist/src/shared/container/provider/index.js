18d7b3d204efefcfaee9a37835437a0977f8d2b8
unknown <fernandoselvino@gmail.com>
2020-09-12 21:53:25 -0300

    build realizado

---
+++ dist/src/shared/container/provider/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./storageProvider");
+require("./mailTemplateProvider");
+require("./mailProvider");
+require("./CacheProvider");
